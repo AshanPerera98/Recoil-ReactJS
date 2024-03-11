48fca11ec2491fbce7d90be76c806687779a3305
Ashan Perera <ashantiwankaperera@gmail.com>
2024-03-11 16:53:42 +0530

    add / delete todos UI

---
+++ src/components/Todos.jsx
@@ -0,0 +1,11 @@
+function Todos() {
+  return (
+    <main>
+      <input type="text" id="todoInput" />
+      <button>Add Todo</button>
+      <button>Delete Todos</button>
+    </main>
+  );
+}
+
+export default Todos;
