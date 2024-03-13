aa01c5b7a37e1571b141f91cb1df23c634aab12e
Ashan Perera <ashantiwankaperera@gmail.com>
2024-03-13 14:54:29 +0530

    todo atom and funtionality implemented

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,9 +1,38 @@
+import { useRecoilState } from "recoil";
+import { todoState } from "../state/atoms/Todo";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 function Todos() {
+  const [todos, setTodos] = useRecoilState(todoState);
+  const [inputText, setInputText] = useState("");
+
+  function addTodo() {
+    setTodos((prevTodos) => [...prevTodos, { id: uuidv4(), text: inputText }]);
+    setInputText("");
+  }
+
+  function clearTodos() {
+    setTodos([]);
+  }
+
+  function handleInputChnage(e) {
+    setInputText(e.target.value);
+  }
+
+  console.log(todos);
+
   return (
     <main>
-      <input type="text" id="todoInput" />
-      <button>Add Todo</button>
-      <button>Delete Todos</button>
+      <input
+        value={inputText}
+        placeholder="Enter todo item"
+        type="text"
+        id="todoInput"
+        onChange={handleInputChnage}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      <button onClick={clearTodos}>Delete Todos</button>
     </main>
   );
 }
